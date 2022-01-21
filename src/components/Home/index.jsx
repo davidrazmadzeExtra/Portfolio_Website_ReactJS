52242d4d7d877169cb3ce1b7b1003fc437ae19f5
David Razmadze <razd45@gmail.com>
2022-01-21 13:24:31 -0500

    Create Headline (Home) Section

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import "./style.css";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Section</h1>
+    <div className="home">
+      <Container className="headline">
+        <Row>
+          {/* Headline - This is what people will see first  */}
+          <Col>
+            <h1>Hi, I'm John.</h1>
+            <h2>I love to build amazing apps.</h2>
+            <p>
+              A little paragraph about yourself. Keep it simple, short, and
+              straight to the point. Use 3-5 sentences in this section.
+              Highlight your current position and skillset.
+            </p>
+            {/* Action Buttons */}
+            <Col>
+              <Button variant="success" style={{ marginRight: "8px" }}>
+                Work With Me
+              </Button>
+              <Button variant="secondary">See My Past Work</Button>
+            </Col>
+          </Col>
+          {/* Picture of Yourself - people will want to see what you look like  */}
+          <Col md={3}>
+            <img
+              src="images/me.png"
+              alt="Picture of Me"
+              className="img-fluid my-image"
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
